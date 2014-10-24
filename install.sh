d2f594ec644596f7a4e2ea99d83dccc8af0504d7
Arnaud Lamy <arnaud.lamy@gmail.com>
2014-10-24 19:05:40 +0200

    Add bundle in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 config_dir=`pwd`/`dirname $0`
 ln -s $config_dir/.vimrc.local $config_dir/..
+ln -s $config_dir/.vimrc.bundles.local $config_dir/..
 ln -s $config_dir/.zsh $config_dir/..
 ln -s $config_dir/.zshrc $config_dir/..
